de274337921bdeb5fcc76220e3ba1b48aa0893a1
namph-comartek <hayvuilen082@gmail.com>
2022-03-07 23:40:51 +0700

    Updates

---
+++ precache-manifest.fc2c098a55187872edcce125fd520bfa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767a9af32d10295b8df6411a0cb7973d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4d845820a5986d6828a",
+    "url": "/static/css/main.c4e46d16.chunk.css"
+  },
+  {
+    "revision": "489b935193938ea196db",
+    "url": "/static/js/2.fde6bb34.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.fde6bb34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d845820a5986d6828a",
+    "url": "/static/js/main.82ec26c9.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "66001cf811f9f101c450edb126e29c63",
+    "url": "/static/media/img-7.66001cf8.jpg"
+  }
+]);
